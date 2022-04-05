339589c4178a41d0e7012a0d8c27295493b2e95a
Romain <ducrot.romain@gmail.com>
2022-04-05 16:16:56 +0200

    Corrections mineures

--- script.py
+++ script.py
@@ -2,10 +2,17 @@ from utils import *
 
 url = 'http://books.toscrape.com/'
 
-categories_list = scrap_categories_list(url)
+books = []
 
-for category in categories_list:
-    books_list = scrap_books_list(url, category)
+categories_url_list = scrap_categories_list(url)
 
-    for book in books_list:
-        scrap_a_book(book)
+#categories_url_list = ['catalogue/category/books/paranormal_24/index.html']
+
+for category_url in categories_url_list:
+    books_url_list = scrap_books_list(url, category_url)
+
+    for book_url in books_url_list:
+        book = scrap_a_book(book_url)
+        books.append(book)
+
+[print(book) for book in books]
--- utils.py
+++ utils.py
@@ -3,13 +3,11 @@ from bs4 import BeautifulSoup
 
 
 def scrap_a_book(url):
-    book_url = url
-
     # TODO:try/catch
-    page = requests.get(book_url)
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    book_info = {
+    return {
         'universal_product_code': soup.find_all('td')[0].text,
         'title': soup.h1.text,
         'price_including_tax': soup.find_all('td')[3].text,
@@ -22,23 +20,20 @@ def scrap_a_book(url):
         'image_url': soup.img['src'].replace('../..', url)
     }
 
-    [print(key + ": " + value) for (key, value) in book_info.items()]
 
-
-def scrap_books_list(url, category):
-    category_url = url + category
+def scrap_books_list(url, category_url):
     # TODO: try/catch
-    page = requests.get(category_url)
+    page = requests.get(url + category_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     book_list_raw = soup.find_all('h3')
-    print(book.a['href'].replace('../../../', url + 'catalogue/') for book in book_list_raw)
     return [book.a['href'].replace('../../../', url + 'catalogue/') for book in book_list_raw]
 
 
 def scrap_categories_list(url):
+    # TODO: try/catch
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     categories_list_raw = soup.find(class_='nav').find_all('a')
-    # On supprime la première catégorie qui contient tous les livres
+    # On supprime la première catégorie qui contient toutes les autres catégories
     del categories_list_raw[0]
     return [category['href'] for category in categories_list_raw]
